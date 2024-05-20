9a0d30f8178880ec4a46735562023f6a03a8bae3
Oleksandr <alex0071396@gmail.com>
2024-05-20 19:00:42 +0300

    task completed

--- homework_5.1/oleksandr_bachurskyi_homework_5.1.py
+++ homework_5.1/oleksandr_bachurskyi_homework_5.1.py
@@ -9,3 +9,39 @@
 # Список зареєстрованих слів можна взяти із keyword.kwlist.
 # У результаті перевірки на друк виводиться або True, якщо таке ім'я змінної допустимо, або False - якщо ні.
 # Приклади імен змінних та результат перевірки (=> на друк виводити не потрібно :))
+
+import string
+import keyword
+
+while True:
+    name = input("Будь ласка, введіть рядок, який ви б хотіли перевірити як можливе ім'я змінної. "
+                 "Для виходу введіть 'exit': ")
+
+    if name == "exit":
+        print("Програма завершила свою роботу.")
+        break
+
+    else:
+        allowed_symbols = string.ascii_letters + string.digits + "_"
+
+        if name[0].isdigit():
+            print(False)
+            continue
+
+        if any(symbol.isupper() for symbol in name):
+            print(False)
+            continue
+
+        if any(symbol not in allowed_symbols for symbol in name):
+            print(False)
+            continue
+
+        if name.count("_") > 1:
+            print(False)
+            continue
+
+        if name in keyword.kwlist:
+            print(False)
+            continue
+
+        print(True)
